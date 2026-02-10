4f0c79b32b432f52057627cd486cf2acfa3404d9
Jan Malinski <contact@janmalinski.com>
2026-02-10 12:45:36 +0100

    use expo-constans to pass firebase account details from .env

--- app.config.ts
+++ app.config.ts
@@ -37,6 +37,7 @@ const config: ExpoConfig = {
   name: getEnviromentalValues().name,
   slug: "firebaseapp",
   version: "1.0.0",
+  runtimeVersion: "1.0.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "firebaseapp",
@@ -111,20 +112,31 @@ const config: ExpoConfig = {
   extra: {
     router: {},
     eas: {
-      // projectId: "74cd81d9-03f9-4ecf-89ee-abd63d0b8559",
       "projectId": "0b277b1f-4a79-44e8-a4f8-bccf0b15d583"
     },
   },
-
-  runtimeVersion: {
-    policy: "appVersion",
-  },
+  // runtimeVersion: {
+  //   policy: "appVersion",
+  // },
 
   updates: {
     url: "https://u.expo.dev/0b277b1f-4a79-44e8-a4f8-bccf0b15d583",
   },
 
-  owner: "jan-malinski-uslugi-programistyczne-spolka-z-oo"
+  owner: "jan-malinski-uslugi-programistyczne-spolka-z-oo",
 };
 
-export default config;
+export default ({ config }: {config: ExpoConfig}) => ({
+  ...config,
+  extra: {
+    ...config.extra,
+
+    firebaseApiKey: process.env.FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.FIREBASE_APP_ID,
+    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+  },
+});
